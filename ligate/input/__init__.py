8e60537d1a48855194015b3c2f7197eb229f14e6
Jakub Beránek <berykubik@gmail.com>
2022-09-01 21:26:33 +0200

    Remove obsolete files

--- ligate/input/__init__.py
+++ ligate/input/__init__.py
@@ -1,41 +1,11 @@
 import dataclasses
-import enum
-from pathlib import Path
-from typing import List
-
-
-class Protein(enum.Enum):
-    Bace = enum.auto()
-
-    def __repr__(self) -> str:
-        return self.name
-
-
-class ForceField(enum.Enum):
-    Amber = enum.auto()
-
-    def __repr__(self) -> str:
-        return self.name
-
-
-Mutation = str
 
 
 @dataclasses.dataclass
 class ComputationTriple:
-    protein: Protein
-    mutation: Mutation
-    forcefield: ForceField
+    protein: str
+    mutation: str
+    forcefield: str
 
     def __repr__(self) -> str:
         return f"(protein {self.protein}, mutation {self.mutation}, forcefield {self.forcefield})"
-
-
-@dataclasses.dataclass
-class GeneratedInput:
-    pdb_files: List[Path]
-
-
-class InputProvider:
-    def provide_input(self, input: ComputationTriple, directory: Path):
-        raise NotImplementedError
--- ligate/input/properties.py
+++
@@ -1,5 +0,0 @@
-from . import ComputationTriple, ForceField
-
-
-def protein_ff(input: ComputationTriple) -> str:
-    return {ForceField.Amber: "amber"}[input.forcefield]
